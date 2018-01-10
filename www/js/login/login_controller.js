3d2c769fc61ca77d5e6c50c1e7c8fafb026228e4
Antonio <ac2@nigithorconsulting.com>
2018-01-10 17:45:21 +0100

    modifiche grafiche

--- www/js/login/login_controller.js
+++ www/js/login/login_controller.js
@@ -15,38 +15,39 @@
         vm.message = '';
 
         vm.login = function () {
-            if(true) {
-                var param = JSON.stringify({username:vm.username,password:vm.password});
-                vm.message = '';
-                $http({
-                    method: 'POST',
-                    url: 'http://'+vm.address+'/mydb/login.php',
-                    data: "message=" + param,
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+
+            var param = JSON.stringify({username:vm.username,password:vm.password});
+            vm.message = '';
+            $http({
+                method: 'POST',
+                url: 'http://'+vm.address+'/mydb/login.php',
+                data: "message=" + param,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            })
+                .success(function (response) {
+                    var input = JSON.parse(response);
+                    var resp = input.records[0].result;
+                    var id = input.records[0].id;
+                    var admin = input.records[0].admin;
+                    if(resp && !admin) {
+                        LoginService.address = vm.address;
+                        LoginService.username = vm.username;
+                        LoginService.password = vm.password;
+                        LoginService.id = id;
+                        LoginService.isLogged = true;
+                        $location.path("/surveys");
+
+                    } else {
+                        vm.username = '';
+                        vm.password = '';
+                        vm.message = "Username or password are incorrect";
+                    }
+
                 })
-                    .success(function (response) {
-                        var input = JSON.parse(response);
-                        var resp = input.records[0].result;
-                        var id = input.records[0].id;
-                        if(resp) {
-                            LoginService.address = vm.address;
-                            LoginService.username = vm.username;
-                            LoginService.password = vm.password;
-                            LoginService.id = id;
-                            LoginService.isLogged = true;
-                            $location.path("/surveys");
+                .error(function (msg) {
+                    vm.message = 'The server address is wrong';
+                });
 
-                        } else {
-                            vm.username = '';
-                            vm.password = '';
-                            vm.message = "Username or password are incorrect";
-                        }
-
-                    })
-                    .error(function (msg) {
-                        vm.message = 'The server address is wrong';
-                    });
-            }
 
         }
     }
